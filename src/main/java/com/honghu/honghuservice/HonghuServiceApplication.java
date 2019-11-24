c8e2fa1bf34007f626889e785685dae1425f9374
caidingwen <caidingwen@lvmama.com>
2019-11-24 15:13:25 +0800

    'init'

---
+++ src/main/java/com/honghu/honghuservice/HonghuServiceApplication.java
@@ -0,0 +1,13 @@
+package com.honghu.honghuservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HonghuServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HonghuServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/honghu/honghuservice/HonghuServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.honghu.honghuservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HonghuServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
